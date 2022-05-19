5ac5d37734eb9de8e3ab3d0c607698ec4a2941cf
natgul <77739919+natgul@users.noreply.github.com>
2022-05-19 13:43:52 +0200

    CSS- Input
    Fixat ett till fält i Input, inkl search + city.

--- group-project/src/RestaurantList.js
+++ group-project/src/RestaurantList.js
@@ -28,11 +28,12 @@ export default function RestaurantList() {
         <div className="container">
             <div className="d-flex justify-content-start mb-4">
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                    <input type="text" class="form-control" placeholder="Search for Restaurant" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <input type="text" aria-label="City" placeholder="Name City" class="form-control"></input>
+                    <button class="btn btn-outline-dark" type="button" id="button-addon2">Search</button>
                 </div>
 
-
+                {/* Filter By Grade*/}
                 <select className="form-select h-25 w-25 p-2 ms-4" aria-label="SelectGrade">
                     <option defaultValue>Filter by Grade</option>
                     <option value="1">1</option>
