1c5c005e5255ae7db58ac4f71aa7350fc9dbed1e
CarolineKristensen <caroline.kristensen@outlook.com>
2022-05-23 14:50:42 +0200

    små ändringar

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -22,28 +22,7 @@ function App() {
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </li>
             </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-dark" type="submit">Search</button>
-            </form>
           </div>
         </div>
       </nav>
@@ -53,7 +32,7 @@ function App() {
       </div>
       <RestaurantList/>
       <div id="map">
-        <MapContainer id ="mapContainer" center={[55.596, 13.023]} zoom={20} scrollWheelZoom={true}>
+        <MapContainer id ="mapContainer" center={[55.596, 13.023]} zoom={15} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a 
         href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
