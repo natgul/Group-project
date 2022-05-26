78df17be7ed0a5f3e8a657bdbf5e5d70d2378652
CarolineKristensen <caroline.kristensen@outlook.com>
2022-05-26 20:16:39 +0200

    Text-ändring
    ändrade så att restaurangernas namn kommer ovanpå bilderna.

--- group-project/src/Restaurant_li.js
+++ group-project/src/Restaurant_li.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 export default function Restaurant(props) {
     return (
         //Prints out each restaurant with content
-        <li className='card-group bg-light bg-gradient m-1'>
+        <li className='card-group'>
             <div className="card text-center w-4">
-                {/*<div className="card-title">
-                    <Link to={'/restaurant/' + props.item.id}> {props.item.name} </Link>
-    </div>*/}
                 <img src={props.item.image_url} className="card-img-top h-100" alt="Bild på mat" />
+                {<div className="card-title">
+                    <Link to={'/restaurant/' + props.item.id}> {props.item.name} </Link>
+                </div>}
 
             </div>
         </li>
