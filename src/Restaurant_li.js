a348ca4145725b16cf50f4b353361747b9d451ef
Mesam97 <mesam.hosein@outlook.com>
2022-06-03 19:27:22 +0200

    Tog bort importer som inte används längre

--- src/Restaurant_li.js
+++ src/Restaurant_li.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 export default function Restaurant(props) {
     return (
         //Returnerar en bild och namnet för varje restaurang från API:et
 
-        //Behövs måsvingarna på rad 12 och 14?
         <li className='card-group mx-auto mt-3'>
             <div className="card text-center">
                 <a href={"/restaurant/" + props.item.id}>
